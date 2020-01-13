d243d9bca2c34cb6678616b8c3449d4abe8ad1cd
Gabriela Vilela <gabrielathalitasv@gmail.com>
2020-01-13 13:55:17 -0300

    nodejs REST

---
+++ controllers/pagamentos.js
@@ -0,0 +1,18 @@
+module.exports = function(app) {
+
+app.get('/pagamentos', function(req, res) {
+  console.log('Recebida requisição de teste na porta 3000')
+  res.send('OK.');
+});
+
+app.post('/pagamentos/pagamento', function(req, res) {
+  var pagamento = req.body;
+  console.log('Processando uma requisição de um novo pagamento.');
+
+pagamento.status = 'CRIADO';
+pagamento.data = new Date;
+
+  res.send(pagamento);
+});
+
+}
